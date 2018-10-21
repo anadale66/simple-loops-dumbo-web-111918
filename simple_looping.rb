499397d36f317e681728b5d0bc098257e385d32b
Roman Pirag <mr.anadale@yahoo.com>
2018-10-21 00:52:20 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,10 +1,6 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(number_of_times)
- 
 
-  end
-end
 
 def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
